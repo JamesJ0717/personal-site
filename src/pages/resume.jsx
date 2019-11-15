e3108f8eed9bd2989e3e5b60846799410f19af53
James Johnson <me@jamesjohnson.io>
2019-11-15 12:12:20 -0500

    s3

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -6,9 +6,7 @@ export default class Resume extends Component {
     return (
       <Layout>
         <embed
-          src={
-            "https://raw.githubusercontent.com/JamesJ0717/resume/560b01b0bdc3eff261acae23f2e12adf918e681a/JJ_Resume.pdf#toolbar=0"
-          }
+          src={"https://s3.amazonaws.com/jj-resume.pdf/JJ_Resume.pdf#toolbar=0"}
           title="Resume"
           style={{ height: "1300px", width: "100%" }}
         />
