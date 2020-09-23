ebb910c6dfa74e978f6c12d572bd0f754c9ab644
James Johnson <me@jamesjohnson.io>
2020-09-23 18:45:28 -0400

    Fixed missing Resume :wrench:

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -5,7 +5,7 @@ export default () => {
   return (
     <Layout>
       <embed
-        src="https://s3.amazonaws.com/jj-resume.pdf/JJ_Resume.pdf#toolbar=0"
+        src="https://render.githubusercontent.com/view/pdf?commit=4a3d5a299a313f75537f707463c2a3db6a415d87&enc_url=68747470733a2f2f7261772e67697468756275736572636f6e74656e742e636f6d2f4a616d65734a303731372f726573756d652f346133643561323939613331336637353533376637303734363363326133646236613431356438372f4a4a5f526573756d652e706466&nwo=JamesJ0717%2Fresume&path=JJ_Resume.pdf&repository_id=213758104&repository_type=Repository#316b0eee-9a29-47fd-a5e3-4db874a28b02#toolbar=0"
         title="Resume"
       />
       <p></p>
